722d9b312747e6bf0bfecff58750fe658cb4929a
inkvizitor68sl <inkvizitor68sl@yandex-team.ru>
2013-04-06 19:49:09 +0400

    * there are now amount of days to keep mysqldumps as variable * all options commented

--- backup-scripts/rdiff-backup.sh
+++ backup-scripts/rdiff-backup.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 
 set_vars () {
-chroot_dest=/
+### There are we can configure some options. By default you should do nothing there, until you know what to do ;)
+
+# which dir should we backup. To be universal script we are backuping / (all files on server).
+files_to_backup=/
+
+# mysql databases list
 db_list=$(chroot ${chroot_dest}/ /usr/bin/mysql -e 'show databases;' | egrep -v '("+--"|Database|mysql|information_schema|performance_schema)')
+
+# backup date/time - will figure at mysqldumps name.
 backupdate=$(date +%Y%m%d-%H%M)
+
 backup_dest=/backup
-backup_dest_chroot=${backup_dest}/rootfs/
+
+# where should we keep files backup. 
+backup_dest_files=${backup_dest}/files/
+
+# where should we keep mysqldumps
 backup_dest_mysqldumps=${backup_dest}/mysqldumps/
+
+# exclude list - this files/dirs will not be backuped by rdiff_backup()
 exclude_list=/etc/backup/exclude.list
+
+# how many days should we keep old mysql dumps. Set amount of days to keep. 
+keep_mysql_dumps=5
+
+### end of configure section.
 }
 
 rdiff_backup () {
-#set -x
-/usr/bin/rdiff-backup --print-statistics --exclude-sockets --preserve-numerical-ids --exclude-globbing-filelist ${exclude_list} ${chroot_dest} ${backup_dest_chroot}
+/usr/bin/rdiff-backup --print-statistics --exclude-sockets --preserve-numerical-ids --exclude-globbing-filelist ${exclude_list} ${files_to_backup} ${backup_dest}
 }
 
 mysql_dumps () {
-#set -x
-# creating dumps:
-#for i in $db_list; do chroot ${chroot_dest} /usr/bin/mysqldump $i > ${backup_dest_mysqldumps}/"$i-${backupdate}".sql; done
-for i in $db_list; do chroot ${chroot_dest} /opt/mysql/server-5.6/bin/mysqldump $i > ${backup_dest_mysqldumps}/"$i-${backupdate}".sql; done
-
+# creating dumps
+for i in $db_list; do mysqldump $i > ${backup_dest_mysqldumps}/"${i}-${backupdate}".sql; done
 
 # gzipping existing dumps:
 for i in `ls ${backup_dest_mysqldumps}*.sql`; do gzip $i; done
 
 # removing backups older, than 5 days
-find ${backup_dest_mysqldumps} -ctime  +5 -exec rm -f {} \;  -print 
+find ${backup_dest_mysqldumps} -ctime  +${keep_mysql_dumps} -exec rm -f {} \;  -print 
 }
 
 set_vars
 rdiff_backup
 mysql_dumps
-
